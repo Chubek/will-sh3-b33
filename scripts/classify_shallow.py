d1ad927a1fdefee8f782199fb1f652c79fdfb740
Chubak <chubakbidpaa@gmail.com>
2021-09-15 02:50:22 +0430

    xgb.DMatrix

--- scripts/classify_shallow.py
+++ scripts/classify_shallow.py
@@ -1,6 +1,6 @@
 from scripts.encode_labels import load_le_status
 from scripts.load_shallow import load_grad_boost, load_svm
-
+import xgboost as  xgb
 
 le_status = load_le_status()
 model_gb = load_grad_boost()
@@ -13,6 +13,6 @@ def classify_svm(input_features):
     return le_status.inverse_transform(pred)[0], int(pred[0])
 
 def classify_gb(input_features):
-    pred = model_gb.predict([input_features])
+    pred = model_gb.predict(xgb.DMatrix([input_features]))
 
     return le_status.inverse_transform(pred)[0], int(pred[0])
