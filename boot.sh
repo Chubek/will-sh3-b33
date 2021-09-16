c61114aaa6483bafb15d780e7afe55ccb817de4b
Chubak <chubakbidpaa@gmail.com>
2021-09-16 06:05:34 +0430

    wc

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gunicorn -b :8000 --access-logfile - --timeout 800 --error-logfile - app:app
+exec gunicorn -b :8000 --worker-class gevent --workers=6 --threads=3 --access-logfile - --timeout 800 --error-logfile - app:app
